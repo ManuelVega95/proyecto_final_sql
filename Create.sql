953d73484eabc34a4ff45b891ed8ac67ce40602a
Manuel Vega <114536071+ManuelVega95@users.noreply.github.com>
2023-06-22 18:49:58 -0300

    Create Create.sql

---
+++ Create.sql
@@ -0,0 +1,7 @@
+-- Creación de Schema y tablas iniciales: --
+
+DROP SCHEMA IF EXISTS Futbol_Proyecto_Final_SQL;
+
+CREATE SCHEMA IF NOT EXISTS Futbol_Proyecto_Final_SQL;
+
+USE Futbol_Proyecto_Final_SQL;
