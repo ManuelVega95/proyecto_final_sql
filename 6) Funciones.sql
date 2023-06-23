dfc57197dc3a3e347574553248f8f683908628a6
Manuel Vega <114536071+ManuelVega95@users.noreply.github.com>
2023-06-23 20:04:04 -0300

    Update 6) Funciones.sql

--- 6) Funciones.sql
+++ 6) Funciones.sql
@@ -13,8 +13,9 @@ END //
 DELIMITER ;
 
 -- SELECT fn_letra_club('') as Clubes_con_X_letra; -- simplemente nos devuelve cuántos clubes hay con la letra que escribamos.
+-- EJEMPLO: SELECT fn_letra_club('a') as Clubes_con_Letra_A;
 
--- SELECT left(Nombre_Club, 1) as Letra, fn_letra_club(left(Nombre_Club, 1)) as Clubes_con_X_Letra from Club; -- un detalle que vi en un tutorial para tener las letras correspondientes y comprobar que la información que se trae es cierta. 
+-- Extra:  SELECT left(Nombre_Club, 1) as Letra, fn_letra_club(left(Nombre_Club, 1)) as Clubes_con_X_Letra from Club; -- un detalle que vi en un tutorial para tener las letras correspondientes y comprobar que la información que se trae es cierta. 
 
 -- Función 2 --
 DROP FUNCTION IF EXISTS fn_paises_clubes;
@@ -28,4 +29,5 @@ BEGIN
 END //
 DELIMITER ;
 
--- SELECT fn_paises_clubes('') as Cantidad_Clubes; -- acá directamente escribimos el nombre de cualquier país y nos devuelve la cantidad exacta de clubes que hay de dicho país en la base de datos. Ejemplo: SELECT fn_paises_clubes('Argentina') as Cantidad_Clubes;
+-- SELECT fn_paises_clubes('') as Cantidad_Clubes; -- acá directamente escribimos el nombre de cualquier país y nos devuelve la cantidad exacta de clubes que hay de dicho país en la base de datos. 
+-- EJEMPLO: SELECT fn_paises_clubes('Argentina') as Cantidad_Clubes; 
