4231b32d70d5d05ad0af5217d6720c54098c77f2
Manuel Vega <114536071+ManuelVega95@users.noreply.github.com>
2023-06-23 19:00:34 -0300

    Update 5) Views.sql

--- 5) Views.sql
+++ 5) Views.sql
@@ -43,3 +43,93 @@ INNER JOIN Club as c ON j.id_Jugador = c.id_Club
 INNER JOIN Estadio as es ON j.id_Jugador = es.id_Estadio
 INNER JOIN Entrenador as en ON j.id_Jugador = en.id_Entrenador
 );
+
+-- View 6 --
+CREATE VIEW vw_Selecciones_Campeonas_del_Mundo as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo and c.Ultimo_Titulo
+ORDER BY c.Titulos DESC
+);
+
+-- View 7 --
+CREATE VIEW vw_Selecciones_Subcampeonas_del_Mundo as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo,
+c.Subcampeonatos
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo and c.Subcampeonatos
+ORDER BY c.Subcampeonatos DESC
+);
+
+-- View 8 --
+CREATE VIEW vw_Selecciones_Terceras_del_Mundo as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo,
+c.Subcampeonatos,
+c.Tercer_Puesto
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo and c.Tercer_Puesto
+ORDER BY c.Tercer_Puesto DESC
+);
+
+-- View 9 --
+CREATE VIEW vw_Selecciones_Cuartas_del_Mundo as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo,
+c.Subcampeonatos,
+c.Tercer_Puesto,
+c.Cuarto_Puesto
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo and c.Cuarto_Puesto
+ORDER BY c.Cuarto_Puesto DESC
+);
+
+-- View 10 --
+CREATE VIEW vw_Selecciones_Top_4_de_Mundiales as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo,
+c.Subcampeonatos,
+c.Tercer_Puesto,
+c.Cuarto_Puesto
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo and c.Titulos and c.Subcampeonatos and c.Tercer_Puesto and c.Cuarto_Puesto
+ORDER BY s.Pais ASC
+);
+
+-- View 11 --
+CREATE VIEW vw_Selecciones_Nacionales as
+(SELECT s.id_Seleccion as Id,
+s.Pais as Seleccion,
+s.Continente,
+s.Confederacion,
+c.Titulos,
+c.Ultimo_Titulo,
+c.Subcampeonatos,
+c.Tercer_Puesto,
+c.Cuarto_Puesto
+FROM Seleccion as s
+INNER JOIN Copas_Del_Mundo as c ON s.id_Seleccion = c.id_Copas_del_Mundo
+ORDER BY s.Pais ASC
+);
