697c9eba723c030156c31098f3a6699a4bc85336
Manuel Vega <114536071+ManuelVega95@users.noreply.github.com>
2023-06-23 17:08:01 -0300

    Update 8) Usuarios.sql

--- 8) Usuarios.sql
+++ 8) Usuarios.sql
@@ -7,7 +7,7 @@
 CREATE USER if not exists 'user_sl'@'localhost' IDENTIFIED BY '12345'; 
 -- el Create User sirve para la creación del usuario 'user_sl'@'localhost', que podrá tener acceso a solo lectura de todas las tablas.
 
-GRANT SELECT ON Futbol.* to 'user_sl'@'localhost';
+GRANT SELECT ON Futbol_Proyecto_Final_SQL.* to 'user_sl'@'localhost';
 -- con este Grant, lo que le permitimos al usuario creado anteriormente 'user_sl'@'localhost', es que pueda tener la vista de todas las tablas del esquema Futbol_Proyecto_Final_SQL.
 -- usando el 'USE Futbol_Proyecto_Final_SQL;' en el nuevo usuario podríamos ver cada tabla del esquema anterior.
 
@@ -16,6 +16,6 @@ GRANT SELECT ON Futbol.* to 'user_sl'@'localhost';
 CREATE USER if not exists 'user_sl_in_up'@'localhost' IDENTIFIED BY '67890';
 -- el Create User sirve para la creación del usuario 'user_sl_in_up'@'localhost', que podrá tener acceso a la lectura, realizar cambios y actualizar en todas las tablas,.
 
-GRANT SELECT, INSERT, UPDATE ON Futbol.* to 'user_sl_in_up'@'localhost';
+GRANT SELECT, INSERT, UPDATE ON Futbol_Proyecto_Final_SQL.* to 'user_sl_in_up'@'localhost';
 -- con este Grant, lo que le permitimos al usuario creado anteriormente 'user_sl_in_up'@'localhost', es que pueda tener la vista de todas las tablas, poder realizar modificaciones y agregar información nueva en el esquema Futbol_Proyecto_Final_SQL.
 -- usando el 'USE Futbol_Proyecto_Final_SQL;' en el nuevo usuario podríamos realizar los SELECT, INSERT y UPDATE del esquema anterior.
